c04a034a089e1d65d409f07e9c8f8e6e5efd6313
YoonJong Choi <83536168+ChoiYoonJong@users.noreply.github.com>
2021-12-22 11:47:56 +0900

    Add files via upload

---
+++ miniProject_3/src/com/uni/run/RunA.java
@@ -0,0 +1,14 @@
+package com.uni.run;
+
+import com.uni.view.PlayMenu;
+
+public class RunA {
+
+	public static void main(String[] args) {
+		
+		PlayMenu pm = new PlayMenu();
+		pm.mainMenu();
+		
+	}
+
+}
