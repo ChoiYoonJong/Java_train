25c613863e5585c5311f0759b6c135c4e600e17b
YoonJong Choi <83536168+ChoiYoonJong@users.noreply.github.com>
2021-12-22 11:31:48 +0900

    java
    java

---
+++ 02. 변수/com/uni/run/Run.java
@@ -0,0 +1,35 @@
+package com.uni.run;
+
+import com.uni.varaible.A_Variable;
+import com.uni.varaible.E_Printf;
+import com.uni.varaible.F_KeyboardInput;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		A_Variable a = new A_Variable();
+		a.test();
+
+//		B_Constant b = new B_Constant();
+//		b.finalConstant();
+
+//		C_Overflow c = new C_Overflow();		
+//		c.printVariableSize();
+//		c.overflow();
+		
+//		D_Cast d = new D_Cast();
+//		d.rule1();
+//		d.rule2();
+//		d.stringTest();
+		
+//		E_Printf e= new E_Printf();
+//		e.printfMethod();
+		
+//		F_KeyboardInput f = new F_KeyboardInput();
+//		//f.inputScanner1();
+//		f.inputScanner3();
+		
+	}
+
+}
---
+++ 07. 객체/uni/chap02_Encapsulation/run/Run.java
@@ -0,0 +1,9 @@
+package com.uni.chap02_Encapsulation.run;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
